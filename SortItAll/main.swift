41682c12ea94b304d84cbc26fdc1dfff80fe60c5
Til Blechschmidt <til@blechschmidt.de>
2019-02-11 11:33:57 +0100

    Initial Commit

---
+++ SortItAll/main.swift
@@ -0,0 +1,12 @@
+//
+//  main.swift
+//  SortItAll
+//
+//  Created by Til Blechschmidt on 11.02.19.
+//  Copyright © 2019 Til Blechschmidt. All rights reserved.
+//
+
+import Foundation
+
+print("Hello, World!")
+
